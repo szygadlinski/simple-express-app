384828139ce2fb23f2e7c7f25174a408b0c02f8f
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-17 03:47:57 +0200

    Add endpoints

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('<h1>My first server!</h1>');
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1>');
+});
+
+app.get('/info', (req, res) => {
+  res.send('<h1>Info</h1>');
+});
+
+app.get('/history', (req, res) => {
+  res.send('<h1>History</h1>');
+});
+
 app.listen(8000, () => {
-  console.log('Server is running: http://localhost:8000');
+  console.log('Server is running on port 8000: http://localhost:8000');
 });
