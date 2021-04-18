d37ec852f99a026c341935fb5c86bc508d8bbd4c
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-18 02:20:36 +0200

    Add views

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.show = name => {
+    res.sendFile(path.join(__dirname, `/views/${name}.html`));
+  };
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('<h1>My first server!</h1>');
+  res.show('index');
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
+  res.show('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>');
+  res.show('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.send('<h1>Info</h1>');
+  res.show('info');
 });
 
 app.get('/history', (req, res) => {
-  res.send('<h1>History</h1>');
+  res.show('history');
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404 - page not found...</h1>');
 });
 
 app.listen(8000, () => {
