4592fad30d5877d683af1c6a649e93d7eaa7b898
Szymon Zygadliński <szygadlinski@gmail.com>
2021-04-18 19:14:12 +0200

    Add layouts

--- server.js
+++ server.js
@@ -4,33 +4,33 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.render('index', { layout:  false });
+  res.render('index', { layout: 'dark' });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout:  false });
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { layout:  false });
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info', { layout:  false });
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.render('history', { layout:  false });
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
 });
 
 app.use((req, res) => {
